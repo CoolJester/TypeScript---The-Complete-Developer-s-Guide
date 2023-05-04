9a2a93384e0c24041a72651d178b58c965587bde
Ntsako Baloyi <ntsako1616@gmail.com>
2023-05-04 08:53:49 +0300

    Add files via upload

---
+++ features/decorators.ts
@@ -0,0 +1,43 @@
+@classDecorator
+class Boat {
+  @testDecorator
+  color: string = 'red';
+
+  @testDecorator
+  get formattedColor():string{
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError
+  pilot(@parameterDecorator speed: string):void{
+    throw new Error();
+    console.log('Swish');
+  }
+}
+
+function classDecorator(constructor: typeof Boat){
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number){
+  console.log(key, index)
+}
+
+function testDecorator(target: any, key: string){
+  console.log(key);
+}
+
+function logError(errorMessage: string){
+
+  return function (target: any, key: string, desc: PropertyDescriptor): void{
+    const method =  desc.value;
+
+    desc.value = function(){
+      try{
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    }
+  }
+}
